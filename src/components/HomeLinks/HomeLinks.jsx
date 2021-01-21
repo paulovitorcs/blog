c32aaf64d9b33d05dd77e67a2ee32f47fc90d97f
Paulo Vitor Cruz <paulocsvitor@gmail.com>
2021-01-20 21:25:58 -0300

    add SimpleGrid to HomeLinks

--- src/components/HomeLinks/HomeLinks.jsx
+++ src/components/HomeLinks/HomeLinks.jsx
@@ -1,16 +1,16 @@
-import { Box, HStack } from "@chakra-ui/react";
+import { Box, SimpleGrid } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import HomeLinkItem from "../HomeLinkItem/HomeLinkItem";
 
 const HomeLinks = () => {
   return (
-    <HStack spacing="75px" justifyContent="center">
+    <SimpleGrid columns={{base: 1, md: 2, lg: 4}} spacing="75px" justifyContent="center">
       <HomeLinkItem icon="rss" headline="Blog" link="/blog" />
       <HomeLinkItem icon={["fab", "github"]} headline="Github" link="https://github.com/paulovitorcs" />
       <HomeLinkItem icon={["fab", "twitter"]} headline="Twitter" link="https://twitter.com/paulovitorcs" />
       <HomeLinkItem icon={["fab", "linkedin"]} headline="LinkedIn" link="https://www.linkedin.com/in/paulovitorcs/" />
-    </HStack>
+    </SimpleGrid>
   );
 };
 
