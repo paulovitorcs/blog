3be8fc22b5e5a5ee8bba401f19c1a328051bbfa8
Paulo Vitor Cruz <paulocsvitor@gmail.com>
2021-01-24 15:58:55 -0300

    fix SEO images and page title

--- src/components/SEO/SEO.jsx
+++ src/components/SEO/SEO.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Helmet } from "react-helmet";
 import { useWpBasicInfo } from "../../static-queries/wp";
+import DefaultThumbnail from '../../../static/default-thumbnail.png';
 
 const SEO = (props) => {
   const { isArticle, title, subtitle, description, url, image } = props;
   const wpBasicInfo = useWpBasicInfo();
-  const imagePage = image ? url : '/default-thumbnail.png';
+  const imagePage = image ? url : DefaultThumbnail;
   const urlPage = url ? process.env.SITE_URL + url : process.env.SITE_URL;
   const titlePage = title ? title : wpBasicInfo.generalSettings.title;
   const subtitlePage = subtitle ? `${titlePage} - ${subtitle}` : null;
@@ -14,18 +15,18 @@ const SEO = (props) => {
     ? description
     : wpBasicInfo.generalSettings.description;
 
-  console.log(imagePage);
+  console.log(subtitlePage);
 
   return (
     <Helmet title={titlePage} titleTemplate={subtitlePage}>
       <meta name="description" content={descriptionPage} />
       {(isArticle ? true : null) && <meta property="og:type" content="article" />}
       <meta property="og:url" content={urlPage} />
-      <meta property="og:title" content={titlePage} />
+      <meta property="og:title" content={subtitle ? subtitle : titlePage} />
       <meta property="og:description" content={descriptionPage} />
       <meta property="og:image" content={imagePage} />
       <meta property="twitter:url" content={urlPage} />
-      <meta property="twitter:title" content={titlePage} />
+      <meta property="twitter:title" content={subtitle ? subtitle : titlePage} />
       <meta property="twitter:description" content={descriptionPage} />
       <meta property="twitter:image" content={imagePage} />
     </Helmet>
