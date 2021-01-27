79e01d0693739b8c04682ef0926b0a5aaeff0abd
Paulo Vitor Cruz <paulocsvitor@gmail.com>
2021-01-27 11:53:16 -0300

    remove twitter from homelinks

--- src/components/HomeLinks/HomeLinks.jsx
+++ src/components/HomeLinks/HomeLinks.jsx
@@ -8,7 +8,6 @@ const HomeLinks = () => {
     <SimpleGrid columns={{base: 1, md: 2, lg: 4}} spacing="75px" justifyContent="center">
       <HomeLinkItem icon="rss" headline="Blog" link="/blog" />
       <HomeLinkItem icon={["fab", "github"]} headline="Github" link="https://github.com/paulovitorcs" />
-      <HomeLinkItem icon={["fab", "twitter"]} headline="Twitter" link="https://twitter.com/paulovitorcs" />
       <HomeLinkItem icon={["fab", "linkedin"]} headline="LinkedIn" link="https://www.linkedin.com/in/paulovitorcs/" />
     </SimpleGrid>
   );
